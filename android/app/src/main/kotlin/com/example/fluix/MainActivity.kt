b22266a81da6dc84c8a93a79ce26e19f4b9e69b8
Tanha Patel <47564751+tanharpatel@users.noreply.github.com>
2021-02-20 11:22:51 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/fluix/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.fluix
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine( flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+
+}
